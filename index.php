09a90f2812d8388ddfbca9e8d92c1c22979781af
Ryan <ryan.arif2000@gmail.com>
2019-04-22 23:32:41 -0500

    Added contact page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if(isset($_GET['home']) || count($_GET) == 0){
 	echo "<div class='w3-row-padding w3-padding-64 w3-container w3-light-grey'>";
 	include 'include/about.html';
 	echo "</div>";
+	include 'include/contact.html';
 	htmlFooter();
 	}
 	//tutoring page
